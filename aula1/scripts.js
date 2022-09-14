2219c66dee08de425da6cd947883a4654e6116b3
Maiala Freitas <101608395+ma-freitass@users.noreply.github.com>
2022-09-14 09:19:17 -0300

    Update scripts.js

--- aula1/scripts.js
+++ aula1/scripts.js
@@ -1,11 +1,18 @@
-function Converter() {
-  var valorElemento = document.getElementById("valor");
-  var valor = valorElemento.value;
-  var valorEmDolarNumerico = parseFloat(valor);
+var nome = "Guilherme"
 
-  var valorEmReal = valorEmDolarNumerico * 5;
-  console.log(valorEmReal);
-  var elementoValorConvertido = document.getElementById("valorConvertido");
-  var valorConvertido = "O resultado em real é R$ " + valorEmReal;
-  elementoValorConvertido.innerHTML = valorConvertido;
-}
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 7
+var notaDoTerceiroBimestre = 4
+var notaDoQuartoBimestre = 2 
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem vindo " + nome)
+console.log(notaFixada)
+
+// isso é um comentário 
+
+// Revisão
+// variáveis, strings, console.log, toFixed, operações matemáticas, concatenação
